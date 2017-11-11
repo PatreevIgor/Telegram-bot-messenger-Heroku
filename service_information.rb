3d56d41b4b89f42ebd6b932f1d4aa707b7ce1867
Patreev Igor <Patreewigor@yandex.ru>
2017-11-11 10:14:13 +0300

    Updating functional awareness

--- service_information.rb
+++ service_information.rb
@@ -1,11 +1,16 @@
 module Information
   USER_ID = ENV['HIDE_USER_ID']
+  items_ids = []
 
   def inform_about_sales
-    if get_items_to_give["success"] == true
-      telegram_bot_client do |bot|
-        bot.api.send_message(chat_id: USER_ID, text: "Item sold!")
-      end
+    if get_items_to_give["success"] == true and validation_send == true
+      send_message if get_items_to_give["success"] == true
+    end
+  end
+
+  def send_message
+    telegram_bot_client do |bot|
+      bot.api.send_message(chat_id: USER_ID, text: "Item sold!")
     end
   end
 
@@ -14,6 +19,27 @@ module Information
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     my_hash = JSON.parse(response.body)
-  end  
+  end
+
+  private
+  def validation_send
+    sold_items.each do |item|
+      if items_ids.include?(item["ui_id"])
+        false
+      else
+        items_ids << item["ui_id"]
+        true
+      end
+    end
+    delete_old_ui_ids
+  end
+
+  def sold_items
+    get_items_to_give["offers"]
+  end
+
+  def delete_old_ui_ids
+    items_ids.рор if items_ids.size >= 20
+  end
 end
 
